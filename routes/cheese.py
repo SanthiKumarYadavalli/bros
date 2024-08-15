2cc5eb2a8b1924ffb3ea453367d4744c55383d58
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-08-15 22:54:43 +0530

    fix: remove face recognition

--- routes/cheese.py
+++ routes/cheese.py
@@ -1,14 +1,14 @@
-import streamlit as st
-import face_recognition
-import utils
+# import streamlit as st
+# import face_recognition
+# import utils
 
-data = utils.get_data()
+# data = utils.get_data()
 
-img = st.camera_input("Cheese!")
-if img:
-    st.image(img)
-    img = face_recognition.load_image_file(img)
-    enc = face_recognition.face_encodings(img)
-    if enc:
-        distances = data['enc'].dropna().apply(lambda e: face_recognition.face_distance([e], enc[0]))
-        st.header(data.loc[distances.idxmin(), "NAME"])
+# img = st.camera_input("Cheese!")
+# if img:
+#     st.image(img)
+#     img = face_recognition.load_image_file(img)
+#     enc = face_recognition.face_encodings(img)
+#     if enc:
+#         distances = data['enc'].dropna().apply(lambda e: face_recognition.face_distance([e], enc[0]))
+#         st.header(data.loc[distances.idxmin(), "NAME"])
