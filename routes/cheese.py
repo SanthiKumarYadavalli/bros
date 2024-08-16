ed41e0d7c3a9e89e1fd35bb094f7407045694056
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-08-16 22:11:15 +0530

    test: remove dlib and add face_recognition fork in requirements.txt

--- routes/cheese.py
+++ routes/cheese.py
@@ -5,10 +5,11 @@ import utils
 data = utils.get_data()
 
 img = st.camera_input("Cheese!")
+img
 if img:
     st.image(img)
-#     img = face_recognition.load_image_file(img)
-#     enc = face_recognition.face_encodings(img)
-#     if enc:
-#         distances = data['enc'].dropna().apply(lambda e: face_recognition.face_distance([e], enc[0]))
-#         st.header(data.loc[distances.idxmin(), "NAME"])
+    img = face_recognition.load_image_file(img)
+    enc = face_recognition.face_encodings(img)
+    if enc:
+        distances = data['enc'].dropna().apply(lambda e: face_recognition.face_distance([e], enc[0]))
+        st.header(data.loc[distances.idxmin(), "NAME"])
