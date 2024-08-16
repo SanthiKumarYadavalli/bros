2bab48aebff6fd9c4fa0fcb616c975c0feae6752
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-08-16 21:21:40 +0530

    test: add environments.yaml

--- routes/cheese.py
+++ routes/cheese.py
@@ -1,12 +1,12 @@
-# import streamlit as st
-# import face_recognition
-# import utils
+import streamlit as st
+import face_recognition
+import utils
 
-# data = utils.get_data()
+data = utils.get_data()
 
-# img = st.camera_input("Cheese!")
-# if img:
-#     st.image(img)
+img = st.camera_input("Cheese!")
+if img:
+    st.image(img)
 #     img = face_recognition.load_image_file(img)
 #     enc = face_recognition.face_encodings(img)
 #     if enc:
